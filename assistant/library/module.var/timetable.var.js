c5fd1e4dbf33488d165610e3fa09eaf8043d2b36
error on line 1 <dev@erroronline.one>
2020-06-05 20:42:52 +0200

    added legal reminder

--- assistant/library/module.var/timetable.var.js
+++ assistant/library/module.var/timetable.var.js
@@ -10,6 +10,10 @@ timetable.var = {
 			en: 'The typed name will be converted to a link to the time table. There is no validation!<br /><br />Enter &quot;Jane Doe&quot; for a sample file.',
 			de: 'Der eingegebene Name wird in einen Link zur Arbeitszeittabelle umgewandelt. Es findet jedoch keine unmittelbare Prüfung auf Korrektheit statt!<br /><br />Versuche &quot;Max Mustermann&quot; als Beispiel.'
 		},
+		legalReminder:{
+			en: 'The access for employees is permitted to the own tables only due to privacy. Opening a foreign timetable is forbidden and allowed to supervisors only!<br /><br /><span class="highlight">By opening the file you confirm your ownership.</span><br /><br />',
+			de: 'Im Sinne des Datenschutzes ist der Zugriff für Mitarbeiter nur auf die eigene Tabelle erlaubt. Das Öffnen einer fremden Arbeitszeiterfassung ist nicht zulässig und ausschließlich Bereichsleitern gestattet!<br /><br /><span class="highlight">Durch das Öffnen der Datei bestätigst du, dass es sich um deine eigene Erfassung handelt.</span><br /><br />'
+		},
 		favouriteCaption: {
 			en: 'quick access',
 			de: 'Schnellzugriff'
