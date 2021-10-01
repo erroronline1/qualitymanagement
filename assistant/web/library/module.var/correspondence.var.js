9dd58903ec1968ed2c9375b1caa95824713af355
erroronline1 <dev@erroronline.one>
2021-10-01 21:30:49 +0200

    reassign var properties to existing property; addressed new data property; omitted useCaseDescription

--- assistant/web/library/module.var/correspondence.var.js
+++ assistant/web/library/module.var/correspondence.var.js
@@ -1,15 +1,5 @@
-if (typeof correspondence === 'undefined') var correspondence = {};
-
 correspondence.var = {
 	lang: {
-		useCaseDescription: {
-			en: 'Select department from above, then the topic, and customize the text using the options. Use direct websearch if you are unsure aabout the name.',
-			de: 'Bitte oben den gewünschten Bereich und anschließend das Thema wählen und den Text über die Optionen anpassen. Bei unklaren Namen direkt die Websuche benutzen.'
-		},
-		inputLoadSubmoduleDefault: {
-			en: 'select department...',
-			de: 'Bereich wählen...'
-		},
 		inputLoadTopicDefault: {
 			en: 'select topic...',
 			de: 'Thema wählen...'
@@ -44,14 +34,13 @@ correspondence.var = {
 		},
 	},
 	submodules: {
-		select: ['', ''],
-		allgemein: ['correspondence_common', 'common'],
+		common: {
+			en: 'common',
+			de: 'Allgemein'
+		},
 	},
 	selectedModule: function () {
-		return el('submodule').options[el('submodule').selectedIndex].value || this.presetModule;
-	},
-	selectedObject: function () {
-		return eval(correspondence.var.selectedModule() + '_data');
+		return el('submodule').options[el('submodule').selectedIndex].value || 'common';
 	},
 	disableOutputSelect: false,
 };
