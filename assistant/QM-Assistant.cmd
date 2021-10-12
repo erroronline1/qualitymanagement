4aa277a3df81b3ef1f7426ee696ce1174d7abbbd
erroronline1 <dev@erroronline.one>
2021-10-12 11:39:42 +0200

    convenient assistants batch start with fallback

---
+++ assistant/QM-Assistant.cmd
@@ -0,0 +1,12 @@
+@echo off
+call assistant --webfolder: "D:\Quality Management\assistant\"
+rem py assistant.py --webfolder: "D:\Quality Management\assistant\"
+
+if %errorlevel% == 1 (
+    echo.
+    echo Error starting wrapper application. Please contact application administration for help.
+    echo About to start web application without wrapper by default, but there might be reduced functionality!
+    echo.
+    pause
+    start "" "D:\Quality Management\assistant\html\core.html"
+)​
