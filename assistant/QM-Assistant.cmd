358b6b61d7c541de0f2511dd526fb19d74cbb7f3
erroronline1 <dev@erroronline.one>
2021-10-13 19:58:08 +0200

    charset because win7

--- assistant/QM-Assistant.cmd
+++ assistant/QM-Assistant.cmd
@@ -1,12 +1,13 @@
 @echo off
-call assistant --webfolder: "D:/Quality Management/assistant/"
+call assistant --webfolder: "D:/Quality Management/assistant/" --browser: Edge
 rem py assistant.py --webfolder: "D:/Quality Management/assistant/"
 
-if %errorlevel% == 1 (
+if %errorlevel% GTR 0 (
     echo.
     echo Error starting wrapper application. Please contact application administration for help.
     echo About to start web application without wrapper by default, but there might be reduced functionality!
+    echo Your default browser must support HTML5/ECMAScript 6+
     echo.
     pause
     start "" "D:\Quality Management\assistant\html\core.html"
-)​
+)
