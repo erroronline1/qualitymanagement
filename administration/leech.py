12c7d4ca427043b21a20e0dea57df6afe2cf9456
erroronline1 <dev@erroronline.one>
2022-03-19 03:43:26 +0100

    added url grabbing on overview sites for easier maintaining of configuration file

--- administration/leech.py
+++ administration/leech.py
@@ -1,6 +1,6 @@
-import json
-import re
-import requests
+'''
+batch download files according to define sites and sourcecode patterns
+'''
 import sys
 import os
 import threading
@@ -9,12 +9,15 @@ import itertools
 import time
 import getpass
 from datetime import date, datetime
+import json
+import re
+import requests
 
 print ('''
      _             _   
     | |___ ___ ___| |_ 
     | | -_| -_|  _|   |
-    |_|___|___|___|_|_| v1.20200530
+    |_|___|___|___|_|_| v1.20220319
 
     by error on line 1 (erroronline.one)
 
@@ -28,6 +31,7 @@ HELPTEXT= '''
 
     usage: leech [ -a | --analyseonly ]               no prompt for download after analyse
                  [ -e | --explicit ] IDENTIFIER       handles identifier group from setup exclusively
+                 [ -j | --json ]                      display configuration json sample
                  [ -h | --help ]                      this message, priority handling
                  [ -r | --results ]                   displays list of detected files urls and names during analysis
                  [ -s | --successlogger ]             logs successful downloads as well
@@ -42,9 +46,12 @@ HELPTEXT= '''
     if proxies are set you will be prompted for the current users password. username and password will be concatenated
     to the proxies adress, if input is not omitted.
     downloaded files will be stored in a source-named and dated subfolder.
-    failure messages will be logged to leech.log-file for further investigation by default.
+    failure messages will be logged to leech.log-file for further investigation by default.'''
 
-    setup is specified in leech.json as following:
+JSONSAMPLE='''
+[JSON]
+
+    configuration is specified in leech.json as following:
 
     {
         "proxies": {
@@ -59,20 +66,23 @@ HELPTEXT= '''
         "folderprefix": "leech_",
         "max_threads": 5,
         "attempts": 3,
-		"timeout": 20,
+        "timeout": 20,
         "sources":{
-            "identifier" : [
-                {
-                    "url": [
-                        "https://www.url.tld/media.html"
-                    ],
-                    "comment": "documents",
-                    "pattern": "files/download/documents/.+?\\\\.pdf",
-                    "filepath": ["https://www.url.tld/",  0],
-                    "filename": ["file_", 0]
-                    "postdata": {...}
-                }
-            ]
+            "identifier" : [{
+				"toplevel": [
+					"https://www.url.tld/overview.html"
+				],
+				"urlpattern": "<article.+?href=\\\"(.+?)\\\"",
+				"urlpath": ["https://www.url.tld/",  0],
+				"url": [
+					"https://www.url.tld/wellknownsubsite.html"
+				],
+				"comment": "documents",
+				"filepattern": "files/download/documents/.+?\\\\.pdf",
+				"filepath": ["https://www.url.tld/",  0],
+				"filename": ["file_", 0]
+				"postdata": {...}
+			}]
         }
     }
 
@@ -86,9 +96,14 @@ HELPTEXT= '''
 
     * identifier optionally describes the data set and serves as name for the destination folder. it may
       contain minus/dash signs but can not be called explicit from the terminal then
-        * url as a list/array contains the pages to be analyze by the same pattern
+        * toplevel as a list/array contains pages to be analyzed for subpages containing files, empty if unnecessary
+        * urlpattern as python flavoured regex to find subpages in pages sourcecode. the pattern will be applied
+          case insensitive and single line
+          remember to escape special characters in regex as well as json-string
+        * urlpath can be a concatenated list of strings and pattern match positions,
+        * url as a list/array contains the pages to be analyzed by the same pattern
         * comment for human comprehension of the dataset only, not actually used by the code
-        * pattern as python flavoured regex to find files in pages sourcecode. the pattern will be applied
+        * filepattern as python flavoured regex to find files in pages sourcecode. the pattern will be applied
           case insensitive and single line
           remember to escape special characters in regex as well as json-string
         * filepath can be a concatenated list of strings and pattern match positions
@@ -97,10 +112,22 @@ HELPTEXT= '''
           handling of one layer of forwarding, login data or whatever can be submitted by a post request
 
     extend whole dictionaries/objects if multiple subsites demand different patterns
+    toplevel sites will be scraped for urls containing actual files that will be added dynamically to the url list
 '''
 
-#load settings
+#init logfile, write session start and parameters for backtracking
+LOGFILE=open('leech.log', 'a')
+LOGFILE.write('\n\nsession on ' + datetime.now().strftime('%Y-%m-%d %H:%M') + ' started with >' + ' '.join(sys.argv) + '\n')
+
+def log(msg):
+	'''log routine writing to file and terminal'''
+	LOGFILE.write( msg + '\n' )
+	sys.stdout.write( '\r' + msg + '\n' )
+	sys.stdout.flush()
+
+
 try:
+	'''load settings'''
 	with open('leech.json', 'r') as jsonfile:
 		SETTINGS= json.loads(jsonfile.read().replace('\n', ''))
 except:
@@ -115,21 +142,11 @@ DOWNLOADED=[]						# future list of successful downloads
 THREAD_COUNTER = 0					# init variable
 ATTEMPT=SETTINGS['attempts']		# init and set variable that will be counted down
 
-TERMINALWIDTH, terminalheight = shutil.get_terminal_size(0)
-terminalheight = 'linter, please ignore unused ' + str(terminalheight)
+TERMINALWIDTH, TERMINALHEIGHT = shutil.get_terminal_size(0)
+TERMINALHEIGHT = 'linter, please ignore unused ' + str(TERMINALHEIGHT)
 
-#init logfile, write session start and parameters for backtracking
-LOGFILE=open('leech.log', 'a')
-LOGFILE.write('\n\nsession on ' + datetime.now().strftime('%Y-%m-%d %H:%M') + ' started with >' + ' '.join(sys.argv) + '\n')
-
-#log routine writing to file and terminal
-def log(msg):
-	LOGFILE.write( msg + '\n' )
-	sys.stdout.write( '\r' + msg + '\n' )
-	sys.stdout.flush()
-
-#fancy anmiation to show something's going on
 def animationbar():
+	'''fancy anmiation to show something's going on'''
 	for c in itertools.cycle(['.   ', '..  ', '... ', '....', ' ...', '  ..', '   .']):
 		if not HIDEANIMATION:
 			sys.stdout.write('\r' + c)
@@ -138,8 +155,8 @@ def animationbar():
 		else:
 			time.sleep(1)
 
-#retrieve source code
 def get_source( link, postdata ):
+	'''retrieve source code'''
 	try:
 		cookies=''
 		if postdata:
@@ -156,8 +173,9 @@ def get_source( link, postdata ):
 	except:
 		log( '[~] connection error with {0}'.format(link) )
 
-#download file
-def requesthandle(site, file, savedestination ):
+
+def requesthandle(file, savedestination ):
+	'''download file'''
 	global FILELIST
 	global THREAD_COUNTER
 	THREAD_COUNTER += 1
@@ -188,18 +206,56 @@ def requesthandle(site, file, savedestination ):
 		log ('[~] general error downloading {0} in attempt {1} of {2}'.format(errorcase, SETTINGS['attempts'] - ATTEMPT + 1, SETTINGS['attempts']))
 	THREAD_COUNTER -= 1
 
-#analyze sourcecode and create file list according to matches
-def analyze(site, src, viewsource):
+def graburls(site, index, viewsource):
+	'''analyze sourcecode and add to urls list according to matches'''
+	src = SETTINGS['sources'][site][index]
+	for url in src['toplevel']:
+		try:
+			ressource = get_source( url, src['postdata'] if 'postdata' in src else False )
+			if (isinstance(viewsource, str) and url == viewsource ) or (isinstance(viewsource, bool) and viewsource):
+				sys.stdout.write( '\r     ' + ressource[0] + '\n' )
+				sys.stdout.flush()
+			urls = re.findall(src['urlpattern'], ressource[0], re.IGNORECASE | re.DOTALL)
+		except:
+			continue
+		if len(urls) < 1:
+			log('[~] no subsites found on [{0}] {1}'.format(site, url))
+			sys.stdout.write( '\r     still analyzing source [' + site + ']' )
+			sys.stdout.flush()
+		for url in urls:
+			urlpath = ''
+			if not isinstance(url, tuple):
+				#make an iterable tuple anyway even if there is just a single match
+				url=tuple([url])
+			#handle filepath and filename concatenation according to settings
+			for s in src['urlpath']:
+				if isinstance(s, int):
+					urlpath += url[s]
+				else:
+					urlpath += s
+			#strip occasional whitespaces
+			urlpath=urlpath.strip()
+			#...and add to list
+			if urlpath not in SETTINGS['sources'][site][index]['url']:
+				SETTINGS['sources'][site][index]['url'].append( urlpath )
+	if displayresults:
+		for f in SETTINGS['sources'][site][index]['url']:
+			sys.stdout.write( '\r' + str(f) + '\n' )
+			sys.stdout.flush()
+
+def grabfiles(site, index, viewsource):
+	'''analyze sourcecode and create file list according to matches'''
+	src = SETTINGS['sources'][site][index]
 	for url in src['url']:
 		try:
 			ressource = get_source( url, src['postdata'] if 'postdata' in src else False )
-			if (type(viewsource) is str and url == viewsource ) or (type(viewsource) is bool and viewsource):
+			if (isinstance(viewsource, str) and url == viewsource ) or (isinstance(viewsource, bool) and viewsource):
 				sys.stdout.write( '\r     ' + ressource[0] + '\n' )
 				sys.stdout.flush()
-			files = re.findall(src['pattern'], ressource[0], re.IGNORECASE | re.DOTALL)
+			files = re.findall(src['filepattern'], ressource[0], re.IGNORECASE | re.DOTALL)
 		except:
 			continue
-		if not len(files):
+		if len(files) < 1:
 			log('[~] no files found on [{0}] {1}'.format(site, url))
 			sys.stdout.write( '\r     still analyzing source [' + site + ']' )
 			sys.stdout.flush()
@@ -212,12 +268,12 @@ def analyze(site, src, viewsource):
 				file=tuple([file])
 			#handle filepath and filename concatenation according to settings
 			for s in src['filepath']:
-				if type(s) is int:
+				if isinstance(s, int):
 					filepath += file[s]
 				else:
 					filepath += s
 			for s in src['filename']:
-				if type(s) is int:
+				if isinstance(s, int):
 					filename += '_' + re.sub(r'(?![# \.-])\W', '', file[s].split('/')[-1] )
 				else:
 					filename += s
@@ -225,7 +281,7 @@ def analyze(site, src, viewsource):
 				filename = filename[1:]
 			#strip occasional whitespaces
 			filepath=filepath.strip()
-			#define result... 
+			#define result...
 			insert=[filepath, filename, ressource[1]] #with ressource[1] being cookies
 			#...and add to list
 			if site in FILELIST:
@@ -238,14 +294,14 @@ def analyze(site, src, viewsource):
 			sys.stdout.write( '\r' + str(f) + '\n' )
 			sys.stdout.flush()
 
-#download according to FILELIST
 def download(site):
+	'''download according to FILELIST'''
 	for file in FILELIST[site]:
 		if not file in DOWNLOADED:
 			savedestination = os.path.join( os.getcwd(), SETTINGS['folderprefix'] + site + '_' + date.today().strftime('%Y%m%d') )
 			if not os.path.isdir(savedestination):
 				os.mkdir( savedestination )
-			_t = threading.Thread( target = requesthandle, args = (site, file, savedestination) )
+			_t = threading.Thread( target = requesthandle, args = (file, savedestination) )
 			_t.daemon = True
 			_t.start()
 			while THREAD_COUNTER >= SETTINGS['max_threads']:
@@ -254,13 +310,14 @@ def download(site):
 		pass
 
 def main(explicit, viewsource):
+	'''main function'''
 	global HIDEANIMATION
 	global FOUNDFILES
 	global ATTEMPT
 
 	print(('\n[!] greedy download' if not analyseonly else 'analyse only') + ' initialized. see leech.log-file to reconstruct results.')
 	print('[!] starting analysis of ' + ( explicit if explicit else str(len(SETTINGS['sources'])) + ' sources' ) + ' on ' + datetime.now().strftime('%Y-%m-%d %H:%M') +'. please stand by.\n')
-	
+
 	animation = threading.Thread(target=animationbar)
 	animation.daemon = True
 	animation.start()
@@ -268,15 +325,19 @@ def main(explicit, viewsource):
 	if explicit:
 		sys.stdout.write( '\r     analyzing source [' + explicit + ']' )
 		sys.stdout.flush()
-		for src in SETTINGS['sources'][explicit]:
-			analyze(explicit, src, viewsource)
+		for index, src in enumerate(SETTINGS['sources'][explicit]):
+			if len(src['toplevel']) > 0:
+				graburls(explicit, index, viewsource)
+			grabfiles(explicit, index, viewsource)
 	else:
-		for index, site in enumerate(SETTINGS['sources']):
-			message = '\r     analyzing source ' + str(index + 1) + ' of ' + str(len(SETTINGS['sources'])) + ' [' + site + ']'
+		for number, site in enumerate(SETTINGS['sources']):
+			message = '\r     analyzing source ' + str(number + 1) + ' of ' + str(len(SETTINGS['sources'])) + ' [' + site + ']'
 			sys.stdout.write( message + ' ' * (TERMINALWIDTH - len(message) - 1) )
 			sys.stdout.flush()
-			for src in SETTINGS['sources'][site]:
-				analyze(site, src, viewsource)
+			for index, src in enumerate(SETTINGS['sources'][site]):
+				if len(src['toplevel']) > 0:
+					graburls(site, index, viewsource)
+				grabfiles(site, index, viewsource)
 	count=0
 	for n in FILELIST:
 		count += len(FILELIST[n])
@@ -314,6 +375,7 @@ def main(explicit, viewsource):
 
 if __name__ == '__main__':
 	lhelp = False
+	ljson = False
 	explicit = False
 	confirm = False
 	viewsource = False
@@ -322,9 +384,10 @@ if __name__ == '__main__':
 	successlogger = False
 	useproxy, user, pw = False, '', ''
 
-	#argument handler	
+	#argument handler
 	#options actually ordered by importance
 	options = {'h':'--help|-h',
+		'j':'--json|-j',
 		'e':'(?:--explicit|-e)[:\\s]+([^-]+\\w+)',
 		'v':'(--viewsource|-v)[:\\s]+([^-]+[\\w;]+)*',
 		'r':'--results|-r',
@@ -335,6 +398,10 @@ if __name__ == '__main__':
 		if opt == 'h' and arg:
 			lhelp = True
 			confirm = 'h'
+			break
+		elif opt == 'j' and arg:
+			ljson = True
+			confirm = 'j'
 			break
 		elif opt == 'e' and bool(arg):
 			explicit = arg[0]
@@ -351,25 +418,28 @@ if __name__ == '__main__':
 
 	#auto help if no source file is found
 	if not SETTINGS:
-		lhelp = True
-		confirm = 'h'
+		ljson = True
+		confirm = 'j'
 
-	if not lhelp and not explicit and not analyseonly:
+	if not lhelp and not ljson and not explicit and not analyseonly:
 		confirm = str(input('[?] without specification you are probably going to download several files from {0} sources.\n    that may take an unpredictable amount of time and data volume.\n    type "y" to proceed with analysis, "h" for help, nothing or any other key to abort: '.format(len(SETTINGS['sources']))))
-	
+
 	if confirm in ['h','help'] or lhelp:
 		print (HELPTEXT)
 		input('[?] press enter to quit...')
+	elif confirm in ['j','json'] or ljson:
+		print (JSONSAMPLE)
+		input('[?] press enter to quit...')
 	elif confirm == 'y' or explicit or analyseonly:
 		for p in SETTINGS['proxies']:
-			if len(SETTINGS['proxies'][p]):
+			if len(SETTINGS['proxies'][p]) > 0:
 				useproxy=True
 		if useproxy:
 			print('\n[?] use of proxy detected, login name and password might be necessary!\n    note that wrong values result in connection errors.')
 			user=getpass.getuser()
 			pw=getpass.getpass('    please enter password for "{0}" (hidden): '.format(user))
 			for p in SETTINGS['proxies']:
-				if len(SETTINGS['proxies'][p]) and len(user) and len(pw):
+				if len(SETTINGS['proxies'][p]) > 0 and len(user) > 0 and len(pw) > 0:
 					temp=re.findall(r'(.+?//)(.+)', SETTINGS['proxies'][p], re.IGNORECASE | re.DOTALL)
 					SETTINGS['proxies'][p] = ''.join([temp[0][0],user,':',pw,'@',temp[0][1]])
 		main(explicit, viewsource)
