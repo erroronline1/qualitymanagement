6c7721d60fdb548d9c28edcd8fed0df2784e67cf
erroronline1 <dev@erroronline.one>
2023-02-08 18:53:14 +0100

    added feedback interface, refactored for reimport

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -9,18 +9,6 @@ import sys
 import tkinter as tk
 from tkinter import filedialog
 
-print('''
-             _     _           _
- ___ ___ ___|_|___| |_ ___ ___| |_   _ _ _ ___ ___ ___ ___ ___ ___
-| .'|_ -|_ -| |_ -|  _| .'|   |  _| | | | |  _| .'| . | . | -_|  _|
-|__,|___|___|_|___|_| |__,|_|_|_|   |_____|_| |__,|  _|  _|___|_|
-                                                  |_| |_|          built 20221220
-
-by error on line 1 (erroronline.one)
-
-$ assistant --help    for overview
-''')
-
 HELP = '''
 start this application with options:
 
@@ -30,6 +18,82 @@ start this application with options:
 [ -p | --port ]       optional if issues arise
 
 '''
+
+WEBFOLDER = None
+BROWSER = None
+PORT =11235
+
+#   _     _ _
+#  |_|___|_| |_
+#  | |   | |  _|
+#  |_|_|_|_|_|
+#
+if __name__ == '__main__':
+	print('''
+             _     _           _
+ ___ ___ ___|_|___| |_ ___ ___| |_   _ _ _ ___ ___ ___ ___ ___ ___
+| .'|_ -|_ -| |_ -|  _| .'|   |  _| | | | |  _| .'| . | . | -_|  _|
+|__,|___|___|_|___|_| |__,|_|_|_|   |_____|_| |__,|  _|  _|___|_|
+                                                  |_| |_|          built 20230208
+
+by error on line 1 (erroronline.one)
+
+$ assistant --help    for overview
+''')
+
+	# argument handler
+	if len(sys.argv):
+		sys.argv.pop(0)
+		options = {
+			'h':['--help|-h', None],
+			'w':['--webfolder|-w', '.+'],
+			'b':['--browser|-b', '.+'],
+			'p':['--port|-p', '\d+']
+			}
+		for i in range(0, len(sys.argv)-1):
+			for opt in options:
+				arg = re.findall(options[opt][0], sys.argv[i], re.IGNORECASE)
+				arg2 = re.findall(options[opt][1], sys.argv[i+1], re.IGNORECASE) if options[opt][1] and i + 1 < len(sys.argv) else None
+				if bool(arg2):
+					i += 1
+				if opt == 'h' and arg:
+					print(HELP)
+					exit()
+				elif opt == 'w' and bool(arg) and bool(arg2):
+					WEBFOLDER = str(arg2[0])
+				elif opt == 'b' and bool(arg) and bool(arg2):
+					BROWSER = str(arg2[0]).lower()
+				elif opt == 'p' and bool(arg) and bool(arg2):
+					PORT = str(arg2[0])
+				else:
+					pass
+	else:
+		print(HELP)
+		exit()
+
+if __name__ == '__main__':
+	if WEBFOLDER:
+		eel.init('html') #fldr name for web content
+
+def interface(what):
+	eel.eel_interface(what)
+
+def webroot():
+	return WEBFOLDER
+
+def rootResourcesImport(file):
+	# https://dev.to/bowmanjd/character-encodings-and-detection-with-python-chardet-and-cchardet-4hj7
+	blob = Path(file).read_bytes()
+	detection = cchardet.detect(blob)
+	encoding = detection["encoding"]
+	text = blob.decode(encoding)
+	return text
+
+#                                 _             _ _ _
+#   _____ ___ _____ ___ ___ _ _  | |_ ___ ___ _| | |_|___ ___
+#  |     | -_|     | . |  _| | | |   | .'|   | . | | |   | . |
+#  |_|_|_|___|_|_|_|___|_| |_  | |_|_|__,|_|_|___|_|_|_|_|_  |
+#                          |___|                         |___|
 
 class db_handler:
 	def __init__(self, db):
@@ -88,83 +152,21 @@ class db_handler:
 		self.connection.commit()
 		return True
 
-_database = db_handler(str(Path.home()) +'/qmassistant.db')
-
-WEBFOLDER = None
-BROWSER = None
-PORT =11235
-# argument handler
-sys.argv.pop(0)
-options = {
-	'h':['--help|-h', None],
-	'w':['--webfolder|-w', '.+'],
-	'b':['--browser|-b', '.+'],
-	'p':['--port|-p', '\d+']
-	}
-for i in range(0, len(sys.argv)-1):
-	for opt in options:
-		arg = re.findall(options[opt][0], sys.argv[i], re.IGNORECASE)
-		arg2 = re.findall(options[opt][1], sys.argv[i+1], re.IGNORECASE) if options[opt][1] and i + 1 < len(sys.argv) else None
-		if bool(arg2):
-			i += 1
-		if opt == 'h' and arg:
-			print(HELP)
-			exit()
-		elif opt == 'w' and bool(arg) and bool(arg2):
-			WEBFOLDER = str(arg2[0])
-		elif opt == 'b' and bool(arg) and bool(arg2):
-			BROWSER = str(arg2[0]).lower()
-		elif opt == 'p' and bool(arg) and bool(arg2):
-			PORT = str(arg2[0])
-		else:
-			pass
-
-#   _     _ _
-#  |_|___|_| |_
-#  | |   | |  _|
-#  |_|_|_|_|_|
-#
-
-@eel.expose
-def webroot():
-	return WEBFOLDER
-
-@eel.expose
-def rootResourcesImport(file):
-	# https://dev.to/bowmanjd/character-encodings-and-detection-with-python-chardet-and-cchardet-4hj7
-	blob = Path(file).read_bytes()
-	detection = cchardet.detect(blob)
-	encoding = detection["encoding"]
-	text = blob.decode(encoding)
-	return text
-
-#                                 _             _ _ _
-#   _____ ___ _____ ___ ___ _ _  | |_ ___ ___ _| | |_|___ ___
-#  |     | -_|     | . |  _| | | |   | .'|   | . | | |   | . |
-#  |_|_|_|___|_|_|_|___|_| |_  | |_|_|__,|_|_|___|_|_|_|_|_  |
-#                          |___|                         |___|
-
-@eel.expose
 def	core_memory_clear():
 	return _database.clear()
 
-@eel.expose
 def core_memory_dbSize():
 	return _database.dbSize()
 
-@eel.expose
 def core_memory_delete(name):
 	return _database.delete(name)
 
-@eel.expose
 def core_memory_keyDump():
 	return _database.keyDump()
 
-@eel.expose
 def	core_memory_read(name):
 	return _database.read(name)
 	
-@eel.expose
 def core_memory_write(name, value):
 	_database.write(name, value)
 	return True
@@ -175,11 +177,9 @@ def core_memory_write(name, value):
 #  |_| |_|_|___| |_|_|__,|_|_|___|_|_|_|_|_  |
 #                                        |___|
 
-@eel.expose
 def file_exists(path):
 	return os.path.exists(path)
 
-@eel.expose
 def file_directory(initial = None, title = None, filetypes = None):
 	picker = tk.Tk()
 	picker.attributes("-topmost", True)
@@ -187,14 +187,12 @@ def file_directory(initial = None, title = None, filetypes = None):
 	picker.destroy()
 	return path
 
-@eel.expose
 def file_handler(call):
 	escaped=[]
 	for arg in call:
 		escaped.append(os.path.normpath(arg) if re.search('./', arg) else arg)
 	subprocess.run(escaped)
 
-@eel.expose
 def file_readdir(path):
 	files = []
 	for (dirpath, dirnames, filenames) in os.walk(path):
@@ -202,7 +200,6 @@ def file_readdir(path):
 			files.append([os.path.join(dirpath, filename).replace('\\', '/'),'',''])
 	return files
 
-@eel.expose
 def file_picker(initial = None, title = None, filetypes = None):
 	picker = tk.Tk()
 	picker.attributes("-topmost", True)
@@ -214,7 +211,6 @@ def file_picker(initial = None, title = None, filetypes = None):
 	picker.destroy()
 	return path
 
-@eel.expose
 def file_saveas(initial = None, title = None, filetypes = None):
 	picker = tk.Tk()
 	picker.attributes("-topmost", True)
@@ -226,41 +222,61 @@ def file_saveas(initial = None, title = None, filetypes = None):
 	picker.destroy()
 	return path
 
-#               _     _
-#   _____ ___ _| |_ _| |___ ___
-#  |     | . | . | | | | -_|_ -|
-#  |_|_|_|___|___|___|_|___|___|
-#  eel.exposure does not seem to happen just from imported modules.
-#  therefore exposed functions are placed here, since the main application has to be updated with importing anyway.
-#  specific algorithms may take place in the distinctive module file
+if __name__ == '__main__':
+	#               _     _
+	#   _____ ___ _| |_ _| |___ ___
+	#  |     | . | . | | | | -_|_ -|
+	#  |_|_|_|___|___|___|_|___|___|
+	#  eel.exposure does not seem to happen just from imported modules.
+	#  therefore exposed functions are placed here, since the main application has to be updated with importing anyway.
+	#  specific algorithms may take place in the distinctive module file
 
-import pymodules.qr as qr
-@eel.expose
-def createqrandopenwith(data, openwith, usecase):
-	return qr.create(data, openwith, usecase, file_handler)
-	#passing file_handler to reuse but to avoid recursive import
+	import pymodules.qr as qr
+	def createqrandopenwith(data, openwith, usecase):
+		return qr.create(data, openwith, usecase, file_handler)
+		#passing file_handler to reuse but to avoid recursive import
 
-import pymodules.filter as processfilter
-@eel.expose
-def filter(settings, arguments):
-	return processfilter.filter(settings, arguments)
+	import pymodules.filter as processfilter
+	def filter(settings, arguments):
+		return processfilter.filter(settings, arguments)
 
-import pymodules.stocklist as stocklistfilter
-@eel.expose
-def translate_split(settings, module, arguments):
-	return stocklistfilter.start(settings, module, arguments)
+	import pymodules.stocklist as stocklistfilter
+	def translate_split(settings, module, arguments):
+		return stocklistfilter.start(settings, module, arguments)
 
-#       _           _
-#   ___| |_ ___ ___| |_
-#  |_ -|  _| .'|  _|  _|
-#  |___|_| |__,|_| |_|
-#
+	#       _           _
+	#   ___| |_ ___ ___| |_
+	#  |_ -|  _| .'|  _|  _|
+	#  |___|_| |__,|_| |_|
+	#
+	_database = db_handler(str(Path.home()) +'/qmassistant.db')
+	
+	eel.expose(webroot)
+	eel.expose(rootResourcesImport)
 
-if WEBFOLDER:
-	print ('\nDo not close this window, otherwise the browserview will stop working.\n')
-	eel.init('html') #fldr name for web content
-	eel.start('core.html', port = PORT, mode = BROWSER)
-else:
-	print(HELP)
+	eel.expose(core_memory_clear)
+	eel.expose(core_memory_dbSize)
+	eel.expose(core_memory_delete)
+	eel.expose(core_memory_keyDump)
+	eel.expose(core_memory_read)
+	eel.expose(core_memory_write)
 
-del _database
+	eel.expose(file_exists)
+	eel.expose(file_directory)
+	eel.expose(file_handler)
+	eel.expose(file_readdir)
+	eel.expose(file_picker)
+	eel.expose(file_saveas)
+
+	eel.expose(createqrandopenwith)
+	eel.expose(filter)
+	eel.expose(translate_split)
+
+	if WEBFOLDER:
+		print ('\nDo not close this window, otherwise the browserview will stop working.\n')
+		eel.start('core.html', port = PORT, mode = BROWSER)
+
+	else:
+		print(HELP)
+
+	del _database
